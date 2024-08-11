a0e9a6fa934900fc3e5499186709732792797e27
git4unrealnondev <30579641+git4unrealnondev@users.noreply.github.com>
2024-08-10 23:01:44 -0500

    removed folder and updooted license

--- target/debug/deps/Cantor_Pair-a8bc7dd1b0f48ab4.d
+++
@@ -1,5 +0,0 @@
-/home/linux/rust/Rust-Cantor-Pair/target/debug/deps/libCantor_Pair-a8bc7dd1b0f48ab4.rmeta: src/lib.rs
-
-/home/linux/rust/Rust-Cantor-Pair/target/debug/deps/Cantor_Pair-a8bc7dd1b0f48ab4.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/Cantor_Pair-fb35b910a245df36.d
+++
@@ -1,7 +0,0 @@
-/home/linux/rust/Rust-Cantor-Pair/target/debug/deps/libCantor_Pair-fb35b910a245df36.rmeta: src/lib.rs
-
-/home/linux/rust/Rust-Cantor-Pair/target/debug/deps/libCantor_Pair-fb35b910a245df36.rlib: src/lib.rs
-
-/home/linux/rust/Rust-Cantor-Pair/target/debug/deps/Cantor_Pair-fb35b910a245df36.d: src/lib.rs
-
-src/lib.rs:
